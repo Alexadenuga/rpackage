1a99e09de8fb5fc962242704b4dd68df2306cb4e
Alex Adenuga <alexadenuga@gmail.com>
2023-09-22 16:48:31 -0400

    Just one of those functions package for sparkline plot updated

--- jootf.R
+++ jootf.R
@@ -1,3 +1,38 @@
-jootf <- function(x){
-  x^2
+#' Just_another_sparkline_plot
+#'
+#' @param dat 
+#' @param trans 
+#' @param colo 
+#' @param label_x 
+#' @param label_y 
+#' @param the_title 
+#'
+#' @return
+#' @export
+#'
+#' @examples  the_sparkline_plot(dat=data, trans = 0.3, colo = "red", label_x = "Data point", label_y = "Value", the_title = "Sparkline Plot")
+#' 
+the_sparkline_plot <- function(dat, trans, colo, label_x, label_y, the_title ){
+  plot(1, type = "n", xlim = c(1, nrow(dat)), ylim = c(0, max(dat)), xlab = label_x, ylab = label_y, xaxt = "n")
+  
+  # Plot the first column (X1) as a normal blue line
+  lines(1:nrow(dat), dat[, 1], col = colo)
+  
+  # Plot the remaining columns as transparent blue lines
+  for (i in 2:num_columns) {
+    lines(1:nrow(dat), dat[, i], col = rgb(1, 0, 0, alpha = trans))
+  }
+  
+  # Add a title (optional)
+  title("the_title")
+  
+  # Add axis labels
+  axis(1, at = 1:nrow(dat), labels = 1:nrow(dat))
+  axis(2, at = seq(0, 1, by = 0.25))
+  
+  # Add a legend (optional)
+  legend("topright", legend = colnames(dat), col = c(colo, rep(rgb(1, 0, 0, alpha = 0.5), ncol(dat) - 1)), lty = 1)
 }
+
+
+
